d60dd6024e026645fcb3ac2ef4c8935725203e4d
ravi-9898 <you@example.com>
2024-05-29 15:56:16 -0400

    wrapper interface with stringer

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type book struct {
+	title string
+}
+
+func (b book) String() string {
+	return fmt.Sprint("The title of the book is: ", b.title)
+}
+
+type count int
+
+func (c count) String() string {
+	return fmt.Sprint("The count is: ", strconv.Itoa(int(c)))
+}
+
+func logInfo(s fmt.Stringer) {
+	fmt.Println("Log from stringer wrapper", s.String())
+}
 
 func main() {
-	fmt.Println("Hello Gophers!\n 😊")
+	b := book{
+		title: "DSA",
+	}
+
+	logInfo(b)
+
+	var c count = 10
+
+	logInfo(c)
 }
